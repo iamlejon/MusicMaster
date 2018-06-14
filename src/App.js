26a686714c47aaba9b67cc5d8296a0db03fa46d4
iamlejon <niquelejon@gmail.com>
2018-06-14 18:17:22 -0500

    Added Spotify API search logic

--- src/App.js
+++ src/App.js
@@ -12,7 +12,23 @@ class App extends Component {
 
     search() {
         console.log('this.state', this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        const FETCH_URL = BASE_URL + 'q=' + this.state.query +'&type=artist&limit=1';
+        var accessToken = 'BQCd0TT1_IgrGvwVDR3xQQoF_jFjKyE8Mvsdn2kqPaeSnhyeUY4P4nDRjyjdhA5ZaZpjFLbYYaQ2rgDwgdB31PVEw5wc5l23sJ1F3EckL5zcK4bHhZrCzGC9PGgjQ_JPe01lsF9dms_O8mr7HpCrQL39x8fsezg5&refresh_token=AQBqeydXMFACT_0rjNCCbXYwo-WFsaq_j-uKOHGkCQeC9J__hc6TVuEzAO4Kqw9hZLLyBIdMXbxyiEbBd5VaxM9JzJzCprnQD4pR5HrEf5X1sT9U0KQjh55G61wEq2ohQqM'
+        var myHeaders = new Headers();
 
+        var myOptions = {
+            method: 'GET',
+             headers: {
+                 'Authorization': 'Bearer ' + accessToken
+             },
+             mode: 'cors',
+             cache: 'default'
+        };
+
+        fetch(FETCH_URL, myOptions)
+        .then(response => response.json())
+        .then(json => console.log(json))
     }
     render() {
         return (
